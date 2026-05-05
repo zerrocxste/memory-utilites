497e4902b109bd3ef35e3f07b6972ef7b5291a79
Fedor L. <49279293+zerrocxste@users.noreply.github.com>
2026-05-05 23:39:35 +0600

    Update memory_utils.cpp

--- memory_utils/memory_utils.cpp
+++ memory_utils/memory_utils.cpp
@@ -1,59 +1,8 @@
-#include "../alternative.h"
+#include "memory_utils.h"
 
 namespace memory_utils
 {
 	static HMODULE base;
-
-	HMODULE get_base()
-	{
-		if (!base)
-			base = GetModuleHandle(NULL);
-		return base;
-	}
-
-	DWORD_PTR get_base_address()
-	{
-		return (DWORD_PTR)get_base();
-	}
-
-	bool is_valid_ptr(PVOID ptr)
-	{
-		return (ptr >= (PVOID)0x10000) && (ptr < PTRMAXVAL) && ptr != nullptr && !IsBadReadPtr(ptr, sizeof(ptr));
-	}
-
-	char* read_string(std::vector<DWORD_PTR>address)
-	{
-		return read_pointer<char*>(address);
-	}
-
-	wchar_t* read_wstring(std::vector<DWORD_PTR>address)
-	{
-		return read_pointer<wchar_t*>(address);
-	}
-
-	bool write_string(std::vector<DWORD_PTR>address, char* my_value)
-	{
-		char* ptr = read_pointer<char*>(address);
-
-		if (ptr == NULL)
-			return false;
-
-		strcpy(ptr, my_value);
-
-		return true;
-	}
-
-	bool write_wstring(std::vector<DWORD_PTR> address, wchar_t* my_value)
-	{
-		wchar_t* ptr = read_pointer<wchar_t*>(address);
-
-		if (ptr == NULL)
-			return false;
-
-		lstrcpyW(ptr, my_value);
-
-		return true;
-	}
 
 	DWORD get_module_size(DWORD_PTR address)
 	{
@@ -130,7 +79,7 @@ namespace memory_utils
 			if (auto compare_result = compare_mem(pattern, mask, start, end, pattern_length, scan_speed))
 				return compare_result;
 
-			next_seg:
+		next_seg:
 
 			fix_seg ? start += size_seg : start = start_seg + size_seg;
 		}
@@ -174,48 +123,11 @@ namespace memory_utils
 			if (auto compare_result = compare_mem(pattern, mask, start, end, pattern_length, scan_speed))
 				ret.push_back(compare_result);
 
-			next_seg:
+		next_seg:
 
 			fix_seg ? start += size_seg : start = start_seg + size_seg;
 		}
 
 		return ret;
 	}
-
-	bool patch_instruction(DWORD_PTR instruction_address, const char* instruction_bytes, int sizeof_instruction_byte)
-	{
-		DWORD dwOldProtection = NULL;
-
-		if (VirtualProtect((LPVOID)instruction_address, sizeof_instruction_byte, PAGE_EXECUTE_READWRITE, &dwOldProtection))
-		{
-			memcpy((LPVOID)instruction_address, instruction_bytes, sizeof_instruction_byte);
-
-			if (VirtualProtect((LPVOID)instruction_address, sizeof_instruction_byte, dwOldProtection, &dwOldProtection))
-			{
-				FlushInstructionCache(GetCurrentProcess(), (LPVOID)instruction_address, sizeof_instruction_byte);
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	bool fill_memory_region(DWORD_PTR instruction_address, int byte, int sizeof_instruction_byte)
-	{
-		DWORD dwOldProtection = NULL;
-
-		if (VirtualProtect((LPVOID)instruction_address, sizeof_instruction_byte, PAGE_EXECUTE_READWRITE, &dwOldProtection))
-		{
-			memset((LPVOID)instruction_address, byte, sizeof_instruction_byte);
-			if (VirtualProtect((LPVOID)instruction_address, sizeof_instruction_byte, dwOldProtection, &dwOldProtection))
-			{
-				FlushInstructionCache(GetCurrentProcess(), (LPVOID)instruction_address, sizeof_instruction_byte);
-				return true;
-			}
-		}
-
-		return false;
-	}
 }
-
-
